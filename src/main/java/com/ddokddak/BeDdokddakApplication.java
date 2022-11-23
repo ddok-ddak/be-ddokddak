298a745c3f6b57a6e42661e772fa6d4b51474c1b
HanaHww2 <hanahww2@gmail.com>
2022-11-23 23:40:22 +0900

    Create Project

---
+++ src/main/java/com/ddokddak/BeDdokddakApplication.java
@@ -0,0 +1,13 @@
+package com.ddokddak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeDdokddakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeDdokddakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddokddak/BeDdokddakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddokddak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeDdokddakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
