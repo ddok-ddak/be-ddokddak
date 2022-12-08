cf693beca58d3bdf6862d4d2db4680849fed58c8
HanaHww2 <hanahww2@gmail.com>
2022-12-08 23:47:22 +0900

    #1 개발을 위한 기본 시큐리티 설정 추가

---
+++ src/main/java/com/ddokddak/common/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.ddokddak.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용하지 않음, 토큰 방식
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeHttpRequests((authz) -> authz
+                        .antMatchers("/", "/css/**").permitAll()
+
+                        // TODO 아래 두 접근에 대해서 관리자만 접근 가능하도록 제한 설정을 해둘 필요성!
+                        // actuator 라이브러리 추가할지 의논해볼 필요
+                        .antMatchers("/actuator/health", "/h2-console/**").permitAll()
+                        .antMatchers("/docs/**", "/v3/api-docs", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
---
+++ src/test/java/com/ddokddak/category/api/CategoryControllerTest.java
@@ -0,0 +1,37 @@
+package com.ddokddak.category.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Slf4j
+@AutoConfigureMockMvc
+@SpringBootTest
+class CategoryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @WithMockUser
+    @DisplayName("카테고리 아이디에 따라 해당 카테고리를 비활성화 데이터로 변경")
+    @Test
+    void deleteCategoryById() throws Exception {
+        log.debug("================test================");
+
+        mockMvc.perform(delete("/api/v1/categories/3"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andDo(print());
+
+    }
+}
