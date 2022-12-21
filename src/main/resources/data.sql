01d6e792e97f5301902d33c3f28c24090831e799
HanaHww2 <hanahww2@gmail.com>
2022-12-21 21:59:37 +0900

    공통 코드 일부 수정 및 추가
    - 시큐리티 설정에서 모든 url 허용 추가
    - 프로젝트 실행시 data.sql 수행하여 기본 데이터 추가 (불필요시
      application.yml 수정해주세요)
    - test 수행시 디비 초기화를 돕는 클래스 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into member (created_at, modified_at) values (now(), now());
+insert into member (created_at, modified_at) values (now(), now());
+
+insert into category(`level`, color, name, delete_yn, parent_id, member_id, created_at, modified_at) values (0, 'test', 'test', 'N', null, 1, now(), now());
+insert into category(`level`, color, name, delete_yn, parent_id, member_id, created_at, modified_at) values (0, 'test', 'test', 'N', null, 1, now(), now());
+insert into category(`level`, color, name, delete_yn, parent_id, member_id, created_at, modified_at) values (0, 'test', 'test', 'N', null, 1, now(), now());
+insert into category(`level`, color, name, delete_yn, parent_id, member_id, created_at, modified_at) values (0, 'test', 'test', 'N', null, 1, now(), now());
