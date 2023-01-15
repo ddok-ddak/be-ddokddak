ec74038094646c4754320895f6eab0f98bfd88f3
HanaHww2 <hanahww2@gmail.com>
2023-01-15 19:35:18 +0900

    DD-29 불필요한 주석 및 파일 삭제

--- src/main/java/com/ddokddak/category/dto/CategoryModifyResponse.java
+++
@@ -1,5 +0,0 @@
-package com.ddokddak.category.dto;
-
-public record CategoryModifyResponse() {
-
-}
--- src/main/java/com/ddokddak/category/dto/CategoryValueModifyRequest.java
+++ src/main/java/com/ddokddak/category/dto/CategoryValueModifyRequest.java
@@ -9,6 +9,6 @@ public record CategoryValueModifyRequest(
         @NotNull String name,
         @NotNull String color
 ) {
-    @Builder // 빌더 어노테이션이 적용되지 않는 이슈로 현재 코드로 작성
+    @Builder
     public CategoryValueModifyRequest {}
 }
