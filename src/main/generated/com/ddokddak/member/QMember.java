a7baa7205a0b07aeccaad1bd00837540240a8c9e
YunDaHyee <7.yundaye.7@gmail.com>
2023-03-22 20:34:55 +0900

    [DD-6] 테스트코드 에러난 것에 대한 수정과 D-11과의 merge

---
+++ src/main/generated/com/ddokddak/member/QMember.java
@@ -0,0 +1,47 @@
+package com.ddokddak.member;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QMember is a Querydsl query type for Member
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMember extends EntityPathBase<Member> {
+
+    private static final long serialVersionUID = -1153133389L;
+
+    public static final QMember member = new QMember("member1");
+
+    public final com.ddokddak.common.entity.QBaseTimeEntity _super = new com.ddokddak.common.entity.QBaseTimeEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public final EnumPath<com.ddokddak.member.entity.TemplateType> templateType = createEnum("templateType", com.ddokddak.member.entity.TemplateType.class);
+
+    public QMember(String variable) {
+        super(Member.class, forVariable(variable));
+    }
+
+    public QMember(Path<? extends Member> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMember(PathMetadata metadata) {
+        super(Member.class, metadata);
+    }
+
+}
+
