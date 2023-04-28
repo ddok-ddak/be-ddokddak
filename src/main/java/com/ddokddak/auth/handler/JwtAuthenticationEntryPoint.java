5726c0f648b9395b00cfb8a90e33582f8d8fe529
HanaHww2 <hanahww2@gmail.com>
2023-04-28 17:23:40 +0900

    공통 코드 수정
    - 필터 디렉토리 생성
    - 파일 위치 수정
    - 불필요한 주석, 임포트문 제거
    - 스웨거 config 추가
      - 스웨거 문서에서 @AuthenticationPrincipal 파라미터 제거
      - 헤더 Authorization 토큰 요소 추가

--- src/main/java/com/ddokddak/auth/handler/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/ddokddak/auth/handler/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,4 @@
-package com.ddokddak.auth.util;
+package com.ddokddak.auth.handler;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
